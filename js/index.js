5b27c3110f1006920586f2f0e88447c302892f77
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2022-08-10 18:07:35 -0300

    Terminar Bonus 1

--- js/index.js
+++ js/index.js
@@ -39,37 +39,48 @@ if (hacker1 < hacker2) {
 }
 
 // BONUS 1
-const text = "Aliquam semper sem iaculis, laoreet nulla id, pharetra nisi. Vestibulum egestas, dui sit amet posuere semper, odio libero ultricies lectus, ut consequat dolor nunc a erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis eleifend consequat dolor imperdiet feugiat. Morbi rhoncus ligula nisl, a ullamcorper erat hendrerit in. Vivamus in nulla tempus eros laoreet aliquam. Aenean vitae neque rhoncus nisi pharetra vulputate eu ut ipsum. Mauris suscipit erat turpis, ut rutrum lectus porttitor pharetra. Integer condimentum metus ut ex laoreet tempor. Mauris ullamcorper dapibus nibh, et cursus ligula vehicula ut. Suspendisse potenti. Nullam dictum ligula id aliquet tincidunt. Cras volutpat porta tempor. Nullam vehicula vitae augue vel pharetra. Donec ac orci lectus. Vestibulum vel augue vel eros gravida ullamcorper. Phasellus ligula quam, hendrerit vitae maximus a, condimentum sit amet orci. Pellentesque malesuada, magna quis lobortis tempor, nisl metus efficitur neque, eget egestas velit mauris sit amet ligula. Donec id vulputate orci. Suspendisse tincidunt, felis vitae iaculis ultrices, turpis velit rhoncus ante, et tempus lectus libero at quam. Mauris eu tincidunt libero, eget mattis ex. Duis lorem purus, consectetur id faucibus id, sodales vel lacus. Donec efficitur ex vitae nunc convallis posuere. Suspendisse bibendum semper turpis et sagittis. Etiam non posuere enim. Sed massa justo, porta id semper quis, volutpat a nisl. Donec condimentum a libero sed vestibulum. Nullam ac eros quis felis congue tempus nec et velit. Nam elit arcu, molestie vel metus sit amet, ultrices facilisis lectus. Sed ligula lorem, mollis eget massa a, dapibus tempus risus. Nullam et finibus sapien, id tristique massa. Nulla id viverra orci. Cras consequat iaculis congue. Curabitur tristique dolor turpis, id faucibus orci condimentum vel. Morbi egestas varius faucibus. Nullam elit massa, ornare a blandit ut, consequat a elit. Fusce et enim sit amet diam porta porta in sit amet dolor."
+const text = "Et Aliquam semper sem iaculis, laoreet nulla id, pharetra nisi. Vestibulum egestas, dui sit amet posuere semper, odio libero ultricies lectus, ut consequat dolor nunc a erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis eleifend consequat dolor imperdiet feugiat. Morbi rhoncus ligula nisl, a ullamcorper erat hendrerit in. Vivamus in nulla tempus eros laoreet aliquam. Aenean vitae neque rhoncus nisi pharetra vulputate eu ut ipsum. Mauris suscipit erat turpis, ut rutrum lectus porttitor pharetra. Integer condimentum metus ut ex laoreet tempor. Mauris ullamcorper dapibus nibh, et cursus ligula vehicula ut. Suspendisse potenti. Nullam dictum ligula id aliquet tincidunt. Cras volutpat porta tempor. Nullam vehicula vitae augue vel pharetra. Donec ac orci lectus. Vestibulum vel augue vel eros gravida ullamcorper. Phasellus ligula quam, hendrerit vitae maximus a, condimentum sit amet orci. Pellentesque malesuada, magna quis lobortis tempor, nisl metus efficitur neque, eget egestas velit mauris sit amet ligula. Donec id vulputate orci. Suspendisse tincidunt, felis vitae iaculis ultrices, turpis velit rhoncus ante, et tempus lectus libero at quam. Mauris eu tincidunt libero, eget mattis ex. Duis lorem purus, consectetur id faucibus id, sodales vel lacus. Donec efficitur ex vitae nunc convallis posuere. Suspendisse bibendum semper turpis et sagittis. Etiam non posuere enim. Sed massa justo, porta id semper quis, volutpat a nisl. Donec condimentum a libero sed vestibulum. Nullam ac eros quis felis congue tempus nec et velit. Nam elit arcu, molestie vel metus sit amet, ultrices facilisis lectus. Sed ligula lorem, mollis eget massa a, dapibus tempus risus. Nullam et finibus sapien, id tristique massa. Nulla id viverra orci. Cras consequat iaculis congue. Curabitur tristique dolor turpis, id faucibus orci condimentum vel. Morbi egestas varius faucibus. Nullam elit massa, ornare a blandit ut, consequat a elit. Fusce et enim sit amet diam porta porta in sit amet dolor et."
 
 
 console.log(text.split(' ').length)
 
+// let count = 0
+// for (let i = 0; i < text.length; i++) {
+//     if (text[i] === " ") {
+//         if (text[i+1] === "e") {
+//             if (text[i+2] === "t") {
+//                 if (text[i+3] === " ") {
+//                     count += 1
+//                 }
+//             }
+//         }
+//     }
+// }
+
 let count = 0
-for (let i = 0; i < text.length; i++) {
-    if (text[i] === " ") {
-        if (text[i+1] === "e") {
-            if (text[i+2] === "t") {
-                if (text[i+3] === " ") {
-                    count += 1
-                }
-            }
+let saveIndex = 0
+    for (let i = 0; i < text.length; i++) {
+        if (text.toLowerCase().slice(saveIndex, text.length).includes("aliquam")) {
+            saveIndex += i
+            count += 1
         }
     }
-}
 
 console.log(count)
 
 //BONUS 2
-const phraseToCheck = "race car"
-let reversePhrase = ''
+const phraseToCheck = "A man, a plan, a canal, Panama!"
+let finalPhrase = phraseToCheck.toLowerCase().replaceAll(/[^a-z]/g,"")
+let reversePhrase = ""
 
-for (let i = phraseToCheck.length - 1; i >= 0; i--) {
-    reversePhrase += phraseToCheck[i]
+for (let i = finalPhrase.length - 1; i >= 0; i--) {
+    reversePhrase += finalPhrase[i]
 }
 
-if (reversePhrase.replaceAll(' ', '') === phraseToCheck.replaceAll(' ', '')) {
+if (reversePhrase.toUpperCase() === finalPhrase.toUpperCase()) {
     console.log("It's a palindrome")
 } else {
     console.log("It's not a palindrome")
 }
 
+console.log(reversePhrase, finalPhrase)
